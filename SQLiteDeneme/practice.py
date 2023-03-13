b57920267966ecb81e0bb14149283b0e09811780
Furkan Beyaz <furkan_1582@hotmail.com>
2023-03-13 19:25:40 +0300

    commit
    commit

--- SQLiteDeneme/practice.py
+++ SQLiteDeneme/practice.py
@@ -1,6 +1,9 @@
 import sqlite3
 
+# CONNECT to the DATABASE
 connection=sqlite3.connect("fenerbahce.db")
+
+# CREATE TABLE
 connection.execute("create table if not exists players(id integer primary key, name text, surname text, number text)")
 
 # INSERT
@@ -10,12 +13,21 @@ connection.execute("create table if not exists players(id integer primary key, n
 # connection.execute("insert into players (name, surname, number) values(?,?,?)", 
 #                    ("joshua","king","20")
 #                    )
+# connection.execute("insert into players (name, surname, number, goals) values(?,?,?,?)",
+#                    ("emre","mor","7","4")
+#                    )
+# connection.execute("insert into players (name, surname, number, goals) values(?,?,?,?)",
+#                    ("diego","rossi","11","12")
+#                    )
+
 
 # ALTER
 # connection.execute("alter table players add column goals text")
 
+
 # DELETE
 # connection.execute("delete from players where name='enner'")
+
 
 # SELECT
 # all = connection.execute("select * from players")
@@ -28,6 +40,12 @@ connection.execute("create table if not exists players(id integer primary key, n
 #           "shirt number:",i[3],
 #           "number of goals:",i[4])
 #     print("-------------------")
+
+
+# UPDATE
+# connection.execute(" update players set goals='23' where id='1' ")
+# connection.execute(" update players set goals='6' where id='2' ")
+
 
 connection.commit()
 connection.close()
