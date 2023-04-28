7e335a215c28d9c449f408fb95d895dcb1024129
Furkan Beyaz <furkan_1582@hotmail.com>
2023-04-28 22:27:01 +0300

    commit
    commit

--- fastapi_subatt/main.py
+++ fastapi_subatt/main.py
@@ -3,10 +3,21 @@ from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 # BaseModel, verilerin belli standartlarda gelmesini sağlar
 import psycopg2
+# postgresql için
 
 app = FastAPI()
+# get post put delete işlemleri yapılabilir
 
-# get post put delete
+# postgresql bağlantısı yapıldı
+def get_connection():
+    conn = psycopg2.connect(
+        database="postgres",
+        user="postgres",
+        password="postgre",
+        host="localhost",
+        port="5432"
+    )
+    return conn
 
 @app.get("/")
 def hello():
@@ -15,3 +26,14 @@ def hello():
 @app.get("/merhaba")
 def merhaba():
     return "Merhaba"
+
+@app.get("/get-users")
+def get_users(baglanti = Depends(get_connection)):
+    try:
+        cursor = baglanti.cursor()
+        cursor.execute("select * from users")
+        rows = cursor.fetchall()
+    except:
+        return {"hata":"bağlantı oluşturulamadı"}
+    finally:
+        baglanti.close()
